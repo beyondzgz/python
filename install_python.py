5bb293e543e005cd36eb1877367a0989884a1a76
Morin <18500233360@qq.com>
2019-04-26 09:13:06 +0800

    t

--- install_python.py
+++ install_python.py
@@ -32,7 +32,7 @@ if ret != 0:
     print('解压失败')
     sys.exit(1)
 
-cmd = 'cd '+package+' && ./configure --prefix=/usr/local/pythonx && make && make install'
+cmd = 'cd '+package+' && ./configure --prefix=/usr/local/python && make && make install'
 ret = os.system(cmd)
 if ret != 0:
     print('编译源码失败')
